d3f45bfd7f1eaa3f9ba6511256be324887d8d721
Macsári László Márk <82086688+Macsari475@users.noreply.github.com>
2023-11-16 10:55:40 +0100

    Add files via upload

---
+++ gondoltam egy szamra/1-100/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int gondoltSzam = random.Next(1, 101);
+            int tipp;
+            int probalkozasok = 0;
+
+
+
+            Console.WriteLine("Gondoltam egy számra 1 és 100 között. Próbálja meg kitalálni!");
+
+
+
+            do
+            {
+                Console.Write("Adjon meg egy tippet: ");
+
+                if (int.TryParse(Console.ReadLine(), out tipp))
+
+                {
+
+                    probalkozasok++;
+
+
+
+                    if (tipp < gondoltSzam)
+
+                    {
+                        Console.WriteLine("Túl kicsi!");
+
+                    }
+
+                    else if (tipp > gondoltSzam)
+
+                    {
+                        Console.WriteLine("Túl nagy!");
+
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"Gratulálok! Kitaláltad a számot ({gondoltSzam}) {probalkozasok} próbálkozás után!");
+                    }
+
+                }
+                else
+                {
+
+                    Console.WriteLine("Hibás bemenet. Kérem, adjon meg egy érvényes számot.");
+                }
+
+
+
+            } while (tipp != gondoltSzam);
+        }
+    }
+
+}
+
