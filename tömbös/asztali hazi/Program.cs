b2ce15ed2a2a85e16c674e829d7fd6b3c5fe0a8c
Macsári László Márk <82086688+Macsari475@users.noreply.github.com>
2023-11-16 11:09:45 +0100

    Add files via upload

---
+++ tömbös/asztali hazi/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asztali_hazi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Adja meg a tömb méretét: ");
+
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int[] tomb = new int[n];
+
+
+
+         
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Adja meg a(z) {i + 1}. számot: ");
+                tomb[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+
+            Console.Write("Adjon meg egy egész számot: ");
+
+
+            int keresettSzam = Convert.ToInt32(Console.ReadLine());
+
+
+
+            int szamlalo = 0;
+
+
+
+          
+            for (int i = 0; i < n; i++)
+            {
+
+                if (tomb[i] == keresettSzam)
+                {
+
+
+                    szamlalo++;
+                }
+            }
+
+
+
+            Console.WriteLine($"{keresettSzam} ennyiszer szerepel a tömbben: {szamlalo} alkalommal.");
+
+        }
+    }
+}
+    
