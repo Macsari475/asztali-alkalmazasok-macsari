576d49f2a3a709d0c5125ef68ad1dce073f8254f
Macsári László Márk <82086688+Macsari475@users.noreply.github.com>
2023-11-16 10:58:11 +0100

    Add files via upload

---
+++ szamologep/szamologep/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szamologep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ez egy számológép");
+
+            int osszeg;
+
+            Console.Write("Elso szam:");
+
+            int szam1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masodik szam:");
+
+            int szam2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Milyen muvelet?(/,+,-,*):");
+
+            string muvelet = Console.ReadLine();
+
+
+
+            switch (muvelet)
+            {
+                case "+":
+                    osszeg = szam1 + szam2;
+                    Console.WriteLine("Osszeadás:" + osszeg);
+
+
+
+                    string valasz = Console.ReadLine();
+                    if (szam1 == szam2)
+                    {
+
+                    }
+                    break;
+
+                case "-":
+
+                    osszeg = szam1 - szam2;
+                    Console.WriteLine("Kivonás:" + osszeg);
+                    Console.Write("Akarsz meg szamolni????(igen,nem):");
+                    break;
+
+                case "*":
+
+                    osszeg = szam1 * szam2;
+                    Console.WriteLine("Szorzás:" + osszeg);
+                    Console.Write("Akarsz meg szamolni????(igen,nem):");
+                    break;
+
+                case "/":
+
+                    osszeg = szam1 / szam2;
+                    Console.WriteLine("Osztás:" + osszeg);
+                    Console.Write("Akarsz meg szamolni????(igen,nem):");
+                    break;
+
+                default:
+
+                    Console.WriteLine("Rosszat adtal meg!");
+                    break;
+            }
+            Console.ReadKey();
+
+
+           
+        }
+    }
+}
