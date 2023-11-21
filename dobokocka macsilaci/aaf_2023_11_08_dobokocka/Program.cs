8fd071cce2f0353cb96c382de7673ff60ced470e
Macsári László Márk <82086688+Macsari475@users.noreply.github.com>
2023-11-21 10:13:51 +0100

    Add files via upload

---
+++ dobokocka macsilaci/aaf_2023_11_08_dobokocka/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aaf_2023_11_08_dobokocka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dobasokSzama = 0;
+            Random random = new Random();
+
+            Console.WriteLine("Ez a program bekéri, hogy hány dobásból álljon a játék, majd ez alapján véletlenszerűen 1 és 6 között generál számokat, majd ezeket elmenti egy tömbe, majd megmondja, hogy hányszor nyert az adott illető.");
+            Console.WriteLine("\nHány alkalommal legyen dobás?");
+            dobasokSzama = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+            for (int i = 0; i < dobasokSzama; i++)
+            {
+                int dobas1 = random.Next(1, 7);
+                int dobas2 = random.Next(1, 7);
+                int dobas3 = random.Next(1, 7);
+
+                int dobasokOsszege = dobas1 + dobas2 + dobas3;
+                string nyertes = "";
+
+                if(dobasokOsszege < 10)
+                {
+                    nyertes = "Anni";
+                }
+                else if(dobasokOsszege > 10)
+                {
+                    nyertes = "Panni";
+                }
+
+                Console.WriteLine("Dobás: {0} + {1} + {2} = {3} Nyert: {4}", dobas1, dobas2, dobas3, dobasokOsszege, nyertes);
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
